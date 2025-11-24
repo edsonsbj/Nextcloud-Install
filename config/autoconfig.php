3083f90dc3ad17d134d622f1ffe8ba76c594af4c
edsonsbj <edson_silvasp@hotmail.com>
2025-11-24 09:29:27 -0300

    configuration files autoconfig.php and custom.config.php

---
+++ config/autoconfig.php
@@ -0,0 +1,9 @@
+<?php
+$AUTOCONFIG = array(
+  'dbtype'        => 'mysql',
+  'dbname'        => 'nextcloud',
+  'dbuser'        => 'nextcloud',
+  'dbpass'        => 'DB_PASS_REPLACE',
+  'dbhost'        => 'DB_HOST_REPLACE',
+  'directory'     => '/var/nextcloud_data',
+);
---
+++ config/custom.config.php
@@ -0,0 +1,32 @@
+<?php
+$CONFIG = array (
+  'default_phone_region' => 'BR',
+  'memcache.distributed' => '\OC\Memcache\Redis',
+  'memcache.local' => '\OC\Memcache\APCu',
+  'memcache.locking' => '\OC\Memcache\Redis',
+  'redis' => 
+  array (
+    'host' => 'localhost',
+    'port' => 6379,
+  ),
+  'htaccess.RewriteBase' => '/',
+  'skeletondirectory' => '',
+  'enabledPreviewProviders' =>
+  array (
+    0 => 'OC\Preview\PNG',
+    1 => 'OC\Preview\JPEG',
+    2 => 'OC\Preview\GIF',
+    3 => 'OC\Preview\BMP',
+    4 => 'OC\Preview\XBitmap',
+    5 => 'OC\Preview\Movie',
+    6 => 'OC\Preview\PDF',
+    7 => 'OC\Preview\MP3',
+    8 => 'OC\Preview\TXT',
+    9 => 'OC\Preview\MarkDown',
+    10 => 'OC\Preview\Image',
+    11 => 'OC\Preview\HEIC',
+    12 => 'OC\Preview\TIFF',
+  ),
+  'trashbin_retention_obligation' => 'auto,30',
+  'versions_retention_obligation' => 'auto,30',
+);
